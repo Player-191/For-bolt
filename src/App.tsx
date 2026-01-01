9b78d196d0d5fe62046c77e46729e92809dd11e5
Player-191 <arifullah1237@gmail.com>
2026-01-01 15:59:58 +0400

    Added package.json

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'system-ui, -apple-system, sans-serif'
+    }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ fontSize: '3rem', marginBottom: '1rem', color: '#333' }}>
+          Welcome to For Bolt
+        </h1>
+        <p style={{ fontSize: '1.25rem', color: '#666' }}>
+          Your project is ready to go!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
